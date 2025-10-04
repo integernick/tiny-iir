7869ee89ffbbf9063cd513d52d3a5c97760bc13a
integernick <nikita.bondarev@microavia.com>
2025-10-04 15:55:09 +0400

    Generic platform bugfix

--- platform/generic/type_utils.h
+++ platform/generic/type_utils.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <algorithm>
+#include <cinttypes>
 #include <string>
 
 namespace tiny_iir {
+
+template<typename>
+struct always_false : std::false_type {
+};
 
 template<typename T>
 static void to_double(const T *x, double *x_double, uint32_t num_samples) {
@@ -14,7 +19,7 @@ static void to_double(const T *x, double *x_double, uint32_t num_samples) {
     } else if constexpr (std::is_same_v<T, double>) {
         std::copy(x, x + num_samples, x_double);
     } else {
-        static_assert(false, "Unsupported conversion type");
+        static_assert(always_false<T>::value, "Unsupported conversion type");
     }
 }
 
@@ -28,18 +33,20 @@ static void to_native(const U *x, T *x_native, uint32_t num_samples) {
                 return static_cast<float>(sample);
             });
         } else {
-            static_assert(false, "Unsupported conversion type");
+            static_assert(always_false<T>::value, "Unsupported conversion type");
         }
     } else if constexpr (std::is_same_v<T, double>) {
         if constexpr (std::is_same_v<U, float>) {
-            arm_f64_to_float(x, x_native, num_samples);
+            std::transform(x, x + num_samples, x_native, [](float sample) {
+                return static_cast<double>(sample);
+            });
         } else if constexpr (std::is_same_v<U, double>) {
             std::copy(x, x + num_samples, x_native);
         } else {
-            static_assert(false, "Unsupported conversion type");
+            static_assert(always_false<T>::value, "Unsupported conversion type");
         }
     } else {
-        static_assert(false, "Unsupported conversion type");
+        static_assert(always_false<T>::value, "Unsupported conversion type");
     }
 }
 
@@ -54,22 +61,22 @@ static void multiply(T *x, T *y, T *dst, uint32_t num_samples) {
             return x * y;
         });
     } else {
-        static_assert(false, "Unsupported conversion type");
+        static_assert(always_false<T>::value, "Unsupported conversion type");
     }
 }
 
 template<typename T>
-static void scale(T *x, const T &scale, T *dst, uint32_t num_samples) {
+static void scale(const T *src, const T &scale, T *dst, uint32_t num_samples) {
     if constexpr (std::is_same_v<T, float>) {
-        std::transform(x, x + num_samples, dst, [&scale](float x) {
+        std::transform(src, src + num_samples, dst, [&scale](float x) {
             return x * scale;
         });
     } else if constexpr (std::is_same_v<T, double>) {
-        std::transform(x, x + num_samples, dst, [&scale](double x) {
+        std::transform(src, src + num_samples, dst, [&scale](double x) {
             return x * scale;
         });
     } else {
-        static_assert(false, "Unsupported conversion type");
+        static_assert(always_false<T>::value, "Unsupported conversion type");
     }
 }
 
